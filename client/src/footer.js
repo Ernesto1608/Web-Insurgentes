28a2807f33fca9424c5e5a3da038240c8ce5825c
Miguel <miguel.elizaldem@outlook.com>
2022-05-08 18:14:44 -0500

    YAaaa

--- client/src/footer.js
+++ client/src/footer.js
@@ -1,8 +1,17 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faFacebook, faInstagram, faSpotify, faTwitter, faYoutube} from '@fortawesome/free-brands-svg-icons'
+import "./footer.css"
+
 function Footer() {
     return (
-      <div className="Home">
-        
-      </div>
+        <nav class="navbar">
+        <div class="container-fluid">
+            <a href='https://open.spotify.com/artist/3ukFu8umbFWty86yaubSR6' target="_blank"><FontAwesomeIcon icon={faSpotify} size="lg"/></a> 
+            <a href='https://www.facebook.com/losinsurgentes.mx' target="_blank"><FontAwesomeIcon icon={faFacebook} size="lg"/></a>
+            <a href='https://twitter.com/insurgentesmx' target="_blank"><FontAwesomeIcon icon={faTwitter} size="lg"/></a>
+            <a href='https://www.instagram.com/accounts/login/insurgentes.mx/' target="_blank"><FontAwesomeIcon icon={faInstagram} size="lg"/></a>
+        </div>
+      </nav>
     );
     }
 
--- client/src/nav.js
+++ client/src/nav.js
@@ -1,17 +1,16 @@
 import './nav.css';
-import logo from "./imagenes/isotipo_alt_insurgentes-03.png"
+import nombre from "./imagenes/Nombre.png"
 
 function NavBar() {
   return (
-    <nav class="navbar navbar-dark bg-dark">
+    <nav class="navbar">
     <div class="container-fluid">
       <a class="navbar-brand" href="./Index.js">
-        <img src={logo} alt="logo" width="30" height="24" class="d-inline-block align-text-top"></img>
-        Los Insurgentes</a>
-        <a class="nav-link" href="./Index.js">Inicio</a>
-        <a class="nav-link" href="#">Eventos</a>
-        <a class="nav-link" href="#">Albums</a>
-        <a class="nav-link" href="#">Tienda</a>
+        <img src={nombre} alt="logo" width="150" height="auto" class="d-inline-block align-text-top"></img></a>
+        <a href="./Index.js">Inicio</a>
+        <a  href="#">Eventos</a>
+        <a  href="#">Albums</a>
+        <a  href="#">Tienda</a>
     </div>
   </nav>
   );
